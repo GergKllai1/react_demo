18d10fe88f6713c7ad4d91ec06d51f7d8e57c764
Gregory Kallai <gerg.kallai@gmail.com>
2018-12-05 07:49:23 +0100

    Adds header and footer jsx, imports them to index js

---
+++ src/Footer.jsx
@@ -0,0 +1,14 @@
+import React, { Component }  from "react"
+
+class Footer extends Component {
+
+    render() {
+        return (
+            <footer>
+                <h1> Made with React</h1>
+            </footer>
+        )
+    }
+}
+
+export default Footer
